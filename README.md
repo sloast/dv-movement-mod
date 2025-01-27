7a1616be2caac187da166899374f59e8077a303b
sloast <adaadrobson@gmail.com>
2025-01-27 12:39:50 +0000

    add ground movement

---
+++ README.md
@@ -0,0 +1,35 @@
+Gravity Gun
+===========
+
+Lets you push train cars around, pick them up, and fling them many miles away :)
+
+Based on [template-umm](https://github.com/derail-valley-modding/template-umm)
+
+Installing
+----------
+
+Go to the releases tab on the right, download the latest release, and install it with [UMM](https://www.nexusmods.com/site/mods/21).
+
+Usage
+-----
+
+- Open the radio and go to "Gravity Gun"
+- Click on a car to pick it up
+- Click to release it
+- The car follows the laser pointer of the radio
+- Scroll to move the car closer or further
+- Hold `Ctrl` and scroll to change the force of the gun
+- Middle click to fling the car
+
+Settings
+--------
+
+Has settings for base force, damping, etc.
+"Min offset to derail" changes how hard you need to pull a car upwards for it to derail.
+"Grab at CoM" changes the behaviour of the gun to always pick up cars from their centre of mass (i.e. applies no torque to the car). Enabling this makes it a lot easier to fly by standing ontop of a car and grabbing.
+
+Issues
+------
+
+Don't create issues here, I won't read them.
+Instead, ping `@adaad` in the [Altfuture Discord server](https://discord.com/invite/altfuture).
--- movement_mod/Main.cs
+++ movement_mod/Main.cs
@@ -74,20 +74,44 @@ public static class Main
 public class Settings : ModSettings, IDrawable
 {
 	[Draw("Auto Bunnyhopping")] public bool autoBhop = false;
-	[Draw("sv_accelerate")] public float sv_accelerate = 10f;
-	[Draw("run speed")] public float runSpeed = 10f;
-	[Draw("max_wishspd")] public float maxWishspd = 0.1f;
-	[Draw("ground accel")] public float ground_accelerate = 0.1f;
+	[Draw("sv_airaccelerate")] public float sv_accelerate = 5f;
+	[Draw("run speed")] public float runSpeed = 6f;
+	[Draw("max_wishspd")] public float maxWishspd = 1f;
 	[Draw("movement speed multiplier")] public float speedmult = 1f;
 	[Draw("ABH (portal/hl2)")] public bool abh = false;
-	[Draw("Max ABH spd")] public float maxAbhSpeed = 1000f;
+	[Draw("Max ABH spd")] public float maxAbhSpeed = 250f;
+	[Draw("Mod ground movement")] public bool walk = true;
+	[Draw("Ground accelerate")] public float ground_accelerate = 10f;
+	[Draw("Ground decelerate")] public float ground_decelerate = 5f;
+	[Draw("Static accel factor")] public float static_accel = 1f;
+
+	[Space]
+	[Draw("Reset")] public bool reset = false;
 
 	public override void Save(UnityModManager.ModEntry modEntry)
 	{
 		Save(this, modEntry);
 	}
 
-	public void OnChange() { }
+	public void OnChange()
+	{
+		if (reset)
+		{
+			autoBhop = false;
+			sv_accelerate = 5f;
+			runSpeed = 6f;
+			maxWishspd = 1f;
+			speedmult = 1f;
+			abh = false;
+			maxAbhSpeed = 250f;
+			walk = true;
+			ground_accelerate = 10f;
+			ground_decelerate = 5f;
+			static_accel = 1f;
+
+			reset = false;
+		}
+	}
 }
 
 public static class SvUser
@@ -127,7 +151,7 @@ static class LocomotionInputNonVr_Patch
 	[HarmonyPatch("AxisSmoothing"), HarmonyPostfix]
 	public static void AxisSmoothing_Postfix(float targetSpeed, ref float __result)
 	{
-		if (!CustomFirstPersonController_Patch.previouslyGrounded)
+		if (!CustomFirstPersonController_Patch.previouslyGrounded || Main.settings.walk)
 			__result = targetSpeed;
 	}
 }
@@ -141,6 +165,7 @@ static class CustomFirstPersonController_Patch
 	public static bool previouslypreviouslyGrounded = true;
 	public static float lastCapsuleHeight = -1f;
 	public static bool canMove = false;
+	public static bool reparented = false;
 
 	[HarmonyPatch("CanMove", MethodType.Getter), HarmonyPostfix]
 	public static void CanMove_Postfix(ref bool __result)
@@ -152,14 +177,30 @@ static class CustomFirstPersonController_Patch
 	[HarmonyPatch("CharacterMovement"), HarmonyPostfix]
 	public static void CharacterMovement_Postfix(CustomFirstPersonController __instance, Vector3 ___desiredMove, Vector3 ___m_MoveDir)
 	{
-		if (!__instance.capsule.enabled)
+		if (__instance.isRepositioning)
 		{
 			velocity = Vector3.zero;
 			return;
 		}
 
-		velocity = __instance.capsule.velocity;
+		if (!__instance.capsule.enabled)
+		{
+			__instance.capsule.enabled = true;
+			___m_MoveDir = Vector3.zero;
+		}
+		else if (!reparented)
+		{
+			velocity = __instance.capsule.velocity;
+		}
+
+		if (reparented) reparented = false;
+
 		horizontalVelocity = velocity.xz();
+
+		float runSpeed = Main.settings.runSpeed;
+
+		if (__instance.IsCrouching)
+			runSpeed *= 0.5f;
 
 		if (__instance.m_Jumping || !previouslyGrounded)
 		{
@@ -168,14 +209,7 @@ static class CustomFirstPersonController_Patch
 				__instance.SetCapsuleHeight(1.1f);
 			}
 
-			float runSpeed = Main.settings.runSpeed;
-
-			if (__instance.IsCrouching)
-			{
-				runSpeed *= 0.75f;
-			}
-
-			Vector2 wishVeloc = ___desiredMove.xz().normalized * runSpeed;
+			Vector2 wishVeloc = ___m_MoveDir.xz();
 
 			SvUser.SV_AirAccelerate(wishVeloc, ref horizontalVelocity);
 
@@ -193,6 +227,21 @@ static class CustomFirstPersonController_Patch
 			velocity.z = horizontalVelocity.y;
 			velocity.y = ___m_MoveDir.y;
 		}
+		else if (!__instance.underwater && Main.settings.walk)
+		{
+			Vector2 wishVeloc = ___m_MoveDir.xz();
+
+			float accel = (wishVeloc.magnitude < 0.1f || (Vector2.Dot(wishVeloc.normalized, velocity.normalized) > 0f && wishVeloc.magnitude < velocity.magnitude))
+				? accel = Main.settings.ground_decelerate
+				: accel = Main.settings.ground_accelerate;
+
+			horizontalVelocity = Vector2.Lerp(horizontalVelocity, wishVeloc, Mathf.Clamp01(accel * Time.deltaTime));
+			horizontalVelocity = Vector2.MoveTowards(horizontalVelocity, wishVeloc, accel * Time.deltaTime * Main.settings.static_accel);
+
+			velocity.x = horizontalVelocity.x;
+			velocity.z = horizontalVelocity.y;
+			velocity.y = ___m_MoveDir.y;
+		}
 		else
 		{
 			velocity = ___m_MoveDir;
@@ -202,7 +251,7 @@ static class CustomFirstPersonController_Patch
 		previouslyGrounded = __instance.previouslyGrounded;
 		lastCapsuleHeight = __instance.CapsuleHeight;
 
-		if (canMove)
+		if (!__instance.isRepositioning)
 		{
 			__instance.capsule.Move(velocity * Time.deltaTime);
 		}
@@ -221,8 +270,34 @@ static class CustomFirstPersonController_Patch
 	public static void Update_Prefix(CustomFirstPersonController __instance, ref bool ___crouchJumped)
 	{
 		if (__instance.IsCrouching && !___crouchJumped && !__instance.capsule.isGrounded)
-		{
 			___crouchJumped = true;
+	}
+}
+
+[HarmonyPatch(typeof(CharacterReparenting))]
+public static class CharacterReparenting_Patch
+{
+	[HarmonyPatch(nameof(CharacterReparenting.ReparentTo)), HarmonyPostfix]
+	public static void ReparentTo_Postfix(CharacterReparenting __instance, CharacterController ___charController, Transform target, bool forceReparent = false)
+	{
+		try
+		{
+
+			if (!forceReparent && target == __instance.transform.parent)
+				return;
+
+			bool flag1 = ___charController.transform.parent.TryGetComponent<Rigidbody>(out Rigidbody rb1);
+			bool flag2 = target.TryGetComponent<Rigidbody>(out Rigidbody rb2);
+
+			Vector3 vel1 = flag1 ? rb1.velocity : Vector3.zero;
+			Vector3 vel2 = flag2 ? rb2.velocity : Vector3.zero;
+
+			CustomFirstPersonController_Patch.velocity += vel2 - vel1;
+			CustomFirstPersonController_Patch.reparented = true;
+		}
+		catch (Exception e)
+		{
+			Main.logger.LogException(e);
 		}
 	}
 }
